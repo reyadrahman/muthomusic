fa558c607b5d8338efcdcef325b60aa66f4b6a03
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-07-17 00:45:45 +0600

    Add files via upload

---
+++ app/src/main/java/com/mutho/music/ui/activities/ToolbarListener.java
@@ -0,0 +1,8 @@
+package com.mutho.music.ui.activities;
+
+import android.support.v7.widget.Toolbar;
+
+//Todo: Better name
+public interface ToolbarListener {
+    void toolbarAttached(Toolbar toolbar);
+}
