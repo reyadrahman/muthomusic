a469d2641d3c7434b3d82ae94e0d5695b163f0d8
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-07-26 18:43:07 +0600

    Add files via upload

---
+++ app/src/main/java/com/mutho/music/ui/modelviews/LoadingView.java
@@ -0,0 +1,39 @@
+package com.mutho.music.ui.modelviews;
+
+import android.view.View;
+import android.view.ViewGroup;
+import com.muthomusicapps.recycler_adapter.model.BaseViewModel;
+import com.muthomusicapps.recycler_adapter.recyclerview.BaseViewHolder;
+
+import static com.mutho.music.R.layout.list_item_loading;
+import static com.mutho.music.ui.adapters.ViewType.LOADING;
+
+public class LoadingView extends BaseViewModel<LoadingView.ViewHolder> {
+
+    @Override
+    public int getViewType() {
+        return LOADING;
+    }
+
+    @Override
+    public int getLayoutResId() {
+        return list_item_loading;
+    }
+
+    @Override
+    public ViewHolder createViewHolder(ViewGroup parent) {
+        return new ViewHolder(createView(parent));
+    }
+
+    public static class ViewHolder extends BaseViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public String toString() {
+            return "LoadingView.ViewHolder";
+        }
+    }
+}
