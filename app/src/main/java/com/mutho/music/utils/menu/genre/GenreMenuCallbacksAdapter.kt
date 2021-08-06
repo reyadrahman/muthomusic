b0d01a2566614cb33f13cac74664bf2e79544295
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-07 02:15:02 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/utils/menu/genre/GenreMenuCallbacksAdapter.kt
+++ app/src/main/java/com/mutho/music/utils/menu/genre/GenreMenuCallbacksAdapter.kt
@@ -1,1 +1,27 @@
+package com.mutho.music.utils.menu.genre
 
+import android.widget.Toast
+import com.mutho.music.model.Genre
+import com.mutho.music.ui.fragments.BaseFragment
+import io.reactivex.disposables.CompositeDisposable
+
+class GenreMenuCallbacksAdapter(val fragment: BaseFragment, val disposables: CompositeDisposable) : GenreMenuUtils.Callbacks {
+
+    override fun showToast(message: String) {
+        Toast.makeText(fragment.context, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPlaylistItemsInserted() {
+
+    }
+
+    override fun onQueueItemsInserted(message: String) {
+
+    }
+
+    override fun playNext(genre: Genre) {
+        fragment.mediaManager.playNext(genre.songsObservable) { message -> Toast.makeText(fragment.context, message, Toast.LENGTH_LONG).show() }?.let { disposable ->
+            disposables.add(disposable)
+        }
+    }
+}
