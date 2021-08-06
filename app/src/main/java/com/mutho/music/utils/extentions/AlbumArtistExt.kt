c39c9711cbf1b2ade18c69adb509c5584d979b7a
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-07 02:19:52 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/utils/extentions/AlbumArtistExt.kt
+++ app/src/main/java/com/mutho/music/utils/extentions/AlbumArtistExt.kt
@@ -1,1 +1,35 @@
+package com.mutho.music.utils.extensions
 
+import com.mutho.music.model.AlbumArtist
+import com.mutho.music.model.Song
+import com.mutho.music.utils.ComparisonUtils
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.ArrayList
+import java.util.Comparator
+
+fun AlbumArtist.getSongs(): Single<List<Song>> {
+    return songsSingle
+        .map { songs ->
+            songs.sortWith(Comparator { a, b -> ComparisonUtils.compareInt(b.year, a.year) })
+            songs.sortWith(Comparator { a, b -> ComparisonUtils.compareInt(a.track, b.track) })
+            songs.sortWith(Comparator { a, b -> ComparisonUtils.compareInt(a.discNumber, b.discNumber) })
+            songs.sortWith(Comparator { a, b -> ComparisonUtils.compare(a.albumName, b.albumName) })
+            songs
+        }
+}
+
+fun Single<List<AlbumArtist>>.getSongs(): Single<List<Song>> {
+    return this.flatMapObservable { list -> Observable.fromIterable(list) }
+        .concatMap { artist: AlbumArtist -> artist.getSongs().toObservable() }
+        .reduce(emptyList(),
+            { songs: List<Song>, songs2: List<Song> ->
+                val allSongs = ArrayList<Song>(songs)
+                allSongs.addAll(songs2)
+                allSongs
+            })
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+}
