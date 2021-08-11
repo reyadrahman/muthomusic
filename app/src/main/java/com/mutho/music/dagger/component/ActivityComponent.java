8b5a7c9f805d7d4ce9898e1d6d075b9e8d9607b1
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-11 07:21:12 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/dagger/component/ActivityComponent.java
+++ app/src/main/java/com/mutho/music/dagger/component/ActivityComponent.java
@@ -1,1 +1,33 @@
+package com.mutho.music.dagger.component;
 
+import com.mutho.music.dagger.module.ActivityModule;
+import com.mutho.music.dagger.module.FragmentModule;
+import com.mutho.music.dagger.module.PresenterModule;
+import com.mutho.music.dagger.scope.ActivityScope;
+import com.mutho.music.search.VoiceSearchActivity;
+import com.mutho.music.ui.activities.MainActivity;
+import com.mutho.music.ui.activities.QCircleActivity;
+import com.mutho.music.ui.appwidget.BaseWidgetConfigure;
+import com.mutho.music.ui.settings.SettingsParentFragment;
+import dagger.Subcomponent;
+
+@ActivityScope
+@Subcomponent(modules = {
+        ActivityModule.class,
+        PresenterModule.class
+})
+
+public interface ActivityComponent {
+
+    FragmentComponent plus(FragmentModule module);
+
+    void inject(MainActivity target);
+
+    void inject(BaseWidgetConfigure target);
+
+    void inject(QCircleActivity target);
+
+    void inject(VoiceSearchActivity target);
+
+    void inject(SettingsParentFragment.SettingsFragment target);
+}
