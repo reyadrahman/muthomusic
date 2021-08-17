da89c00d4d55ef266ad8bcb4fdbfb9ea0a647afd
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-17 11:11:43 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/playback/constants/ExternalIntents.java
+++ app/src/main/java/com/mutho/music/playback/constants/ExternalIntents.java
@@ -1,1 +1,18 @@
+package com.mutho.music.playback.constants;
 
+public interface ExternalIntents {
+
+    String PLAY_STATUS_REQUEST = "com.android.music.playstatusrequest";
+
+    String PLAY_STATUS_RESPONSE = "com.android.music.playstatusresponse";
+
+    String AVRCP_PLAY_STATE_CHANGED = "com.android.music.playstatechanged";
+
+    String AVRCP_META_CHANGED = "com.android.music.metachanged";
+
+    String TASKER = "net.dinglisch.android.tasker.extras.VARIABLE_REPLACE_KEYS";
+
+    String SCROBBLER = "com.adam.aslfms.notify.playstatechanged";
+
+    String PEBBLE = "com.getpebble.action.NOW_PLAYING";
+}
