7b459e1c2a85ed7c79f0edd5aa597574f7e3eec5
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-18 00:31:51 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/rx/UnsafeAction.java
+++ app/src/main/java/com/mutho/music/rx/UnsafeAction.java
@@ -1,1 +1,12 @@
+package com.mutho.music.rx;
 
+import io.reactivex.functions.Action;
+
+/**
+ * An Action which does not throw on error.
+ */
+public interface UnsafeAction extends Action {
+
+    @Override
+    void run();
+}
