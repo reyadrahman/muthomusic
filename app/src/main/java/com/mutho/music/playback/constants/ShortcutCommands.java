8046b1f941e3628f8f9f6ac3f26ed349de4d9970
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-17 11:16:30 +0600

    Add files via upload

---
+++ app/src/main/java/com/mutho/music/playback/constants/ShortcutCommands.java
@@ -0,0 +1,8 @@
+package com.mutho.music.playback.constants;
+
+public interface ShortcutCommands {
+    String PLAY = "com.mutho.music.shortcuts.PLAY";
+    String SHUFFLE_ALL = "com.mutho.music.shortcuts.SHUFFLE";
+    String FOLDERS = "com.mutho.music.shortcuts.FOLDERS";
+    String PLAYLIST = "com.mutho.music.shortcuts.PLAYLIST";
+}
