298c78802f734d5f096191b031ab669e550f87e4
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-21 06:15:03 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/constants/Config.java
+++ app/src/main/java/com/mutho/music/constants/Config.java
@@ -1,1 +1,17 @@
+package com.mutho.music.constants;
 
+public final class Config {
+
+    /* This class is never initialized */
+    private Config() {
+    }
+
+    public static final String ID = "id";
+
+    public static final String NAME = "name";
+
+    public static final String PACKAGE_NAME_FREE = "another.music.player";
+    public static final String PACKAGE_NAME_PRO = "com.simplecity.amp_pro";
+
+    public static final String CHROMECAST_APP_ID = "73341C53";
+}
