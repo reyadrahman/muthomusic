9afaf40f2251900c6d6aa312da789c02c1c7e10c
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-21 06:16:42 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/dagger/module/ActivityModule.java
+++ app/src/main/java/com/mutho/music/dagger/module/ActivityModule.java
@@ -1,1 +1,22 @@
+package com.mutho.music.dagger.module;
 
+import android.app.Activity;
+import com.mutho.music.dagger.scope.ActivityScope;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class ActivityModule {
+
+    private Activity activity;
+
+    public ActivityModule(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Provides
+    @ActivityScope
+    Activity provideActivity() {
+        return activity;
+    }
+}
