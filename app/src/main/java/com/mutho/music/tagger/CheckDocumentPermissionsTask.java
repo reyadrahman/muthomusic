99614a0179fd33c436f02b28b3384430ea381e11
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-08-24 13:15:27 +0600

    Add files via upload

--- app/src/main/java/com/mutho/music/tagger/CheckDocumentPermissionsTask.java
+++ app/src/main/java/com/mutho/music/tagger/CheckDocumentPermissionsTask.java
@@ -1,1 +1,37 @@
+package com.mutho.music.tagger;
 
+import android.os.AsyncTask;
+import android.support.v4.provider.DocumentFile;
+import java.util.List;
+
+public class CheckDocumentPermissionsTask extends AsyncTask<Void, Void, Boolean> {
+
+    public interface PermissionCheckListener {
+        void onPermissionCheck(boolean hasPermission);
+    }
+
+    private List<String> paths;
+    private List<DocumentFile> documentFiles;
+
+    private PermissionCheckListener listener;
+
+    public CheckDocumentPermissionsTask(List<String> paths, List<DocumentFile> documentFiles, PermissionCheckListener listener) {
+        this.paths = paths;
+        this.documentFiles = documentFiles;
+        this.listener = listener;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        return !TaggerUtils.requiresPermission(paths) || TaggerUtils.hasDocumentTreePermission(documentFiles, paths);
+    }
+
+    @Override
+    protected void onPostExecute(Boolean hasPermission) {
+        super.onPostExecute(hasPermission);
+
+        if (listener != null) {
+            listener.onPermissionCheck(hasPermission);
+        }
+    }
+}
